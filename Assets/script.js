b87107673e5f072225d78d59f717912890b43b6a
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-01-07 22:37:09 -0500

    Created function to check current time against row value and update CSS

--- Assets/script.js
+++ Assets/script.js
@@ -2,12 +2,11 @@
 var timeDisplayEl = $('#currentDay');
 var nextTaskDisplayEl = $('#nextTask');
 var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-var currentHour = moment().format('hh');
+var currentHour = Number(moment().format('HH'));
 var scheduleHour = document.getElementsByClassName('hour');
 var mySelect = document.getElementById('validationCustom04');
 
 //Displays date & time in header
-
 
 function displayTime() {
     timeDisplayEl.text(rightNow);
@@ -15,50 +14,61 @@ function displayTime() {
 
 setInterval(displayTime,1000);
 
-console.log(mySelect.value)
-//Allow user to set start time and edit Schedule
+// ****BONUS**** Allow user to set start time and edit Schedule
+            // function defineRow() {
+            //     let text = "";
 
-function defineRow() {
-    var scheduleTime = mySelect.value;
-    var rowValue = scheduleHour.createTextNode(scheduleTime);
+            //     for (let i = 0; i<7; i++) {
+            //         text += i;
+            //     };
 
-};
+            //     scheduleHour2.innerHTML = text;
+            //     console.log(text);
+            // };
 
+            // defineRow(); 
+            
+            // mySelect.addEventListener("select", defineRow);
 
-//Set default start time
-var temp = "9";
-
-for(var i, j = 0; i = mySelect.options[j]; j++) {
-    if(i.value == temp) {
-        mySelect.selectedIndex = j;
-        break;
-    };
-}
-
-defineRow();
-mySelect.addEventListener("select", defineRow);
+// ****BONUS**** Set default start time
+        // for(var i, j = 0; i = mySelect.options[j]; j++) {
+        //     var temp = "9";
+        //     if(i.value == temp) {
+        //         mySelect.selectedIndex = j;
+        //         break;
+        //     };
+        // };
 
 //Change Element Style based on current time
+var scheduleHourValue = scheduleHour.innerText;
+
 function checkTime(){
-    if(currentHour > scheduleHour.value){
-        $(".hour").addClass("past");
-    }
-    else if(currentHour === scheduleHour.value){
-        $(".hour").addClass("present");
-    }
-    else{
-        $(".hour").addClass("future");
-    };
+    [...scheduleHour].forEach(
+        function time (scheduleHour) {
+            var nowValue = parseInt(scheduleHour.innerHTML);
+
+            for (let i = 0; i<7; i++) {
+                if(currentHour > nowValue){
+                    $(".hour").addClass("past");
+                }
+                else if(currentHour === nowValue){
+                    $(".hour").addClass("present");
+                }
+                else{
+                    $(".hour").addClass("future");
+                };}
+            console.log(nowValue)
+        }
+    );
 };
 
+checkTime();
+setInterval(checkTime, (1000 * 60) * 5);
 
-//Check time every 5 minutes
-setInterval(checkTime(), (1000 * 60) * 5);  
+// ****BONUS**** Displays time to next task in aside card
+    // function displayNextTask() {
+    //     var nextTask = (timeObj.value - moment().format('hh:mm:ss'));
+    //     nextTaskDisplayEl.text(nextTask);
+    //   }
 
-//Displays time to next task in aside card
-// function displayNextTask() {
-//     var nextTask = (timeObj.value - moment().format('hh:mm:ss'));
-//     nextTaskDisplayEl.text(nextTask);
-//   }
-
-//   setInterval(displayNextTask, 1000);
+    //   setInterval(displayNextTask, 1000);
